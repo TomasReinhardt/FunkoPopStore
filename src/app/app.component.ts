3b3e3d96dbcf75a5fcd48d052ac8d24ab2768ff6
Tomas Reinhardt <tomas_reinhardt@hotmail.com>
2021-09-28 17:55:08 -0300

    alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit{
   public userA: any = {exist:'false',admin:'false'};
 
   ngOnInit(): void { 
-    //alert("La funcion de subir y editar imagen no funciona debido a que el host gratuito que utilizo para montar la api no permite la carga de archivos. \n\nLas imagene fueron subidas mediando la api de forma local, para ver el codigo de subida de imagenes ingresar al repositorio: \n\nhttps://github.com/TomasReinhardt/api-rest-funkopop.git")
+    alert("La funcion de subir y editar imagen no funciona debido a que el host gratuito que utilizo para montar la api no permite la carga de archivos. \n\nLas imagene fueron subidas mediando la api de forma local, para ver el codigo de subida de imagenes ingresar al repositorio: \n\nhttps://github.com/TomasReinhardt/api-rest-funkopop.git")
   }
   click(){
     $('.list-collections').slideToggle();
